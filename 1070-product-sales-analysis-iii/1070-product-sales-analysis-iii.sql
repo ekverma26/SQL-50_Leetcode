61a35ba6969ba1a1516a069c3c239429c4aac2e7
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-03 19:10:45 +0530

    Time: 833 ms (18.64%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,5 +3,5 @@ with cte as
 (select product_id, min(year) as first_year from Sales
 group by product_id)
 
-select c.product_id, first_year, quantity , price from Sales
+select c.product_id, c.first_year, s.quantity , s.price from Sales
 as s inner join cte as c on s.product_id = c.product_id and s.year = c.first_year
