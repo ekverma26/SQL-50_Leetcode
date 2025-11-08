eeac0d5c82788f113e61acba28b246f355db5874
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-08 22:42:55 +0530

    Time: 797 ms (52.13%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -2,4 +2,4 @@
 with cte as(select person_name, turn, sum(weight) over(order by turn) as total_weight
 from Queue)
 
-select person_name from (select * from cte where total_weight<=1000) as t order by turn desc limit 1
+select person_name from cte where total_weight<=1000 order by turn desc limit 1
