e89c0fc67c3c34bed39c496d20cf24b2202a5646
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-14 19:06:29 +0530

    Time: 1274 ms (42.17%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -2,14 +2,12 @@
 on mr.user_id=u.user_id
 group by u.user_id
 order by count(*) desc, name asc limit 1)
+
 union all
-(select results from 
-(
-    select title as results, avg(rating) as a_rating
-    from MovieRating as mr inner join
-    Movies as m
-    on mr.movie_id=m.movie_id
-    where month(created_at)=2
-    group by m.movie_id
-)
-rating_group order by a_rating desc, results asc limit 1)
+
+(select m.title as results from Movies m
+join MovieRating mr on m.movie_id = mr.movie_id
+where year(mr.created_at)=2020 and month(mr.created_at)=02
+group by m.title, m.movie_id
+order by avg(mr.rating) desc, m.title
+limit 1)
