15136f83830b4132033e476c172bb5195d207781
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-17 19:27:26 +0530

    Time: 934 ms (50.77%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte as
+(select e.name as Employee, departmentId,d.name as Department, salary,
+dense_rank() over(partition by d.name order by salary desc) as r
+from Employee as e 
+inner join Department as d on e.departmentId = d.id)
+
+select Department, Employee , salary from cte 
+where r<=3
