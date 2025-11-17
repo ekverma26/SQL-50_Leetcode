65ae3b8ac26c6004538fef5fe08e17eabea52fbd
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-17 19:28:05 +0530

    Time: 1281 ms (18.35%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -3,7 +3,7 @@ with cte as
 (select e.name as Employee, departmentId,d.name as Department, salary,
 dense_rank() over(partition by d.name order by salary desc) as r
 from Employee as e 
-inner join Department as d on e.departmentId = d.id)
+join Department as d on e.departmentId = d.id)
 
 select Department, Employee , salary from cte 
 where r<=3
