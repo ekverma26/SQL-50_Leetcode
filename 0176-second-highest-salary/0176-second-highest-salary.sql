4d3d7c1574cf1ccffdd4aab621301357199803a4
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2025-11-20 18:46:41 +0530

    Time: 258 ms (59.99%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-with cte as(select *,row_number() over(order by salary desc) as rn 
+with cte as(
+select *,dense_rank() over(order by salary desc) as rn 
 from Employee)
 
-select if(max(rn)<2,null, salary) as SecondHighestSalary from cte
+select max(case when rn=2 then salary else null end) as SecondHighestSalary from cte
 where rn=2
